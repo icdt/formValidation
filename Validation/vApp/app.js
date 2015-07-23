d907539b2586d963b28b6fe62439ec3e556eb4f2
istale <iscarryon@gmail.com>
2015-07-23 12:27:19 +0800

    kevin: commit for github

---
+++ Validation/vApp/app.js
@@ -0,0 +1,47 @@
+﻿var app = angular.module('app', [
+    'ui.router',
+    'kendo.directives'
+    //state
+    
+
+]);
+
+app.run(['$rootScope', '$state', '$stateParams',
+        function ($rootScope, $state, $stateParams) {
+
+            $rootScope.$state = $state;
+            $rootScope.$stateParams = $stateParams;
+
+            // 若未驗證成功會有stateChangeError, 判斷error名字，轉到登入會面
+            $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+                if (error.name === 'AuthenticationRequired') {
+                    //User.setNextState(toState.name, 'You must login to access this page');
+                    //alert("Login error, pls check user/pass");
+                    $state.go('login', {}, { reload: true });
+                }
+            });
+
+        }
+]);
+
+
+app.config(['$stateProvider', '$urlRouterProvider', '$httpProvider',
+function ($stateProvider, $urlRouterProvider, $httpProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+    .state('validation', {
+        url: '/',
+        templateUrl: 'appPages/form/validation.html',
+        controller: 'validationCtrl',
+        resolve: {
+            //bodyClass: ['$rootScope', function ($rootScope) {
+            //    $rootScope.bodyClass = 'login-page';
+            //    $rootScope.wrapperClass = 'login-box';
+            //}]
+        }
+    })
+    ;
+
+}]);
---
+++ Validation/vApp/appPages/form/validationCtrl.js
@@ -0,0 +1,109 @@
+﻿app.controller('validationCtrl', ['$scope', '$rootScope', '$state', function ($scope, $rootScope, $state) {
+
+    $scope.vm = {};
+
+    //下拉選單
+    //產品類別
+    $scope.product_category = {
+        dataSource: {
+            data: [{ category: 0, name: '常用' },
+                   { category: 1, name: '預備品' },
+                   { category: 2, name: '熱門銷售' }]
+        },
+        dataTextField: "name",
+        dataValueField: "category",
+        optionLabel: "請選擇產品類別"
+    };
+
+    //數字篩選
+    $("#price_change").kendoNumericTextBox(
+        {
+            culture: "de-DE",
+            value: 0,
+            step: 100
+        });
+
+    //日期篩選
+    //$("#Start_Date").kendoDatePicker({
+    //    culture: "zh-CHT",
+    //    format: "yyyy/MM/dd"
+    //});
+
+    //$("#End_Date").kendoDatePicker({
+    //    culture: "zh-CHT",
+    //    format: "yyyy/MM/dd"
+    //});
+
+    //顯示時間
+    //$("#Show_Time").kendoTimePicker({
+    //});
+
+    //自動完成
+    var dataSource = new kendo.data.DataSource({
+        transport: {
+            read: {
+                url: "http://demos.telerik.com/kendo-ui/service/products",
+                dataType: "jsonp"
+            }
+        }
+    });
+    $("#autocomplete").kendoAutoComplete({
+        dataSource: dataSource,
+        dataTextField: "ProductName"
+    });
+
+    //Tab切換容器
+    $("#tabstrip").kendoTabStrip({
+        animation: { open: { effects: "fadeIn" } },
+        contentUrls: []
+    });
+
+    //單張圖片上傳
+    $("#file").kendoUpload({
+        multiple: false,
+        async: {
+            saveUrl: "save",
+            removeUrl: "remove",
+            autoUpload: false
+        }
+    });
+
+    //多張圖片上傳
+    $("#files").kendoUpload({
+        multiple: true,
+        async: {
+            saveUrl: "save",
+            removeUrl: "remove",
+            autoUpload: false
+        }
+    });
+
+    //備註
+    $("#note").kendoEditor({
+        tools: [
+                 "bold",
+                 "italic",
+                 "underline",
+                 "strikethrough",
+                 "justifyLeft",
+                 "justifyCenter",
+                 "justifyRight",
+                 "justifyFull",
+                 "foreColor",
+                 "backColor"
+        ]
+    });
+
+    //驗證區塊
+    $scope.submitted = false;
+    $scope.signupForm = function () {
+        if ($scope.signup_form.$valid) {
+            $scope.signup_form.submitted = false;
+            alert("儲存成功!!");
+        $scope.vm.time = new Date($scope.vm.time).toLocaleTimeString();
+            console.log($scope.vm);
+        } else {
+            $scope.signup_form.submitted = true;
+        }
+    }
+}]);
